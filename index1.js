177a2a03b0502f4c18482b022167f3d1ef79c158
akbarrsmwn <akbarrismawan7@gmail.com>
2022-06-02 10:40:33 +0700

    commit on master

---
+++ index1.js
@@ -0,0 +1,95 @@
+// function tampilkan(){
+// document.querySelector("p").innerHTML="Judul di klik";
+// //  }
+// function tampilkanClick(){
+//     document.querySelector("p").innerHTML="Judul di klik";
+//     }
+  
+// Event mouse klik dll
+
+// function tampilkanClickh3(){
+//     document.querySelector("h3").innerHTML="Judul di klik";
+//     }
+   
+
+// var h1Node = document.getElementById("judul");
+// h1Node.addEventListener("click",tampilkanClick);
+// h1Node.addEventListener("click",tampilkanClickh3);
+// h1Node.removeEventListener("click",tampilkanClickh3); // remove
+
+// h1Node.addEventListener("dblclick",tampilkanDblclick);
+// h1Node.addEventListener("contextmenu",tampilkanContextmenu);
+// h1Node.addEventListener("mouseover",onMouseover);
+// h1Node.onclick = tampilkanClick; 
+// h1Node.ondblclick = tampilkanDblclick;
+// h1Node.oncontextmenu = tampilkanContextmenu;
+// h1Node.onmouseover = onMouseover;
+
+
+// Event Focus & Blur
+// var usernameNode = document.getElementById("username");
+// var emailNode = document.getElementById("email");
+
+//     function diFocus(e){
+//         e.target.style.border = "5px solid aqua";
+//  }
+//     function diBlur(e){
+//         e.target.style.border = "2px solid red";
+//  }
+
+//  usernameNode.addEventListener("focus",diFocus);
+//  emailNode.addEventListener("focus",diFocus);
+ 
+//  usernameNode.addEventListener("blur",diBlur);
+//  emailNode.addEventListener("blur",diBlur);
+
+// event Change
+//  var usernameNode = document.getElementById("username");
+
+//  function diChange(e){
+//     e.target.style.border = "5px solid yellow";
+// }
+// usernameNode.addEventListener("change",diChange);
+
+
+//form 
+var usernameNode = document.getElementById("username");
+var emailNode = document.getElementById("email");
+
+    function diFocus(e){
+        e.target.style.border = "5px solid aqua";
+ }
+    function diBlur(e){
+        e.target.style.border = "2px solid red";
+ }
+
+ usernameNode.addEventListener("focus",diFocus);
+ emailNode.addEventListener("focus",diFocus);
+ 
+ usernameNode.addEventListener("blur",diBlur);
+ emailNode.addEventListener("blur",diBlur);
+
+var focusUNode = document.getElementById("focusU")
+var focusENode = document.getElementById("focusE")
+
+focusUNode.addEventListener("click",function(){
+    usernameNode.focus();
+}); 
+
+focusENode.addEventListener("click",function(){
+    usernameNode.focus();
+});
+
+
+//password
+// var passwordNode = document.getElementById("password");
+// var tombolPassNode = document.getElementById("tombol");
+
+// function proses(){
+//     passwordNode.type = "text";
+//  }
+
+//  tombolPassNode.addEventListener("click",proses);
+
+
+
